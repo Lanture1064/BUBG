f88c3c079afc989a7ec65de58ab15e1cbcd4b7d1
ZhangBolun <7578973@qq.com>
2018-05-26 21:29:08 +0800

    增加设置场景
    增加设置场景，点击“设置”（目前图标为“how to play”）进入设置场景，方式为“push”。
    可设置音效与声音的开启与否，目前只有图标。点击“OK”返回起始菜单。

---
+++ Classes/SettingScene1.cpp
@@ -0,0 +1,97 @@
+#include "SettingScene1.h"
+
+USING_NS_CC;
+
+Scene* Setting::createScene()
+{
+	// 'scene' is an autorelease object
+	auto scene = Scene::create();
+
+	// 'layer' is an autorelease object
+	auto layer = Setting::create();
+
+	// add layer as a child to scene
+	scene->addChild(layer);
+
+	// return the scene
+	return scene;
+}
+
+// on "init" you need to initialize your instance
+bool Setting::init()
+{
+	//////////////////////////////
+	// 1. super init first
+	if (!Layer::init())
+	{
+		return false;
+	}
+
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	Sprite *bg = Sprite::create("menu/background.png");
+
+	// position the label on the center of the screen
+	bg->setPosition(Vec2(origin.x + visibleSize.width / 2,
+		origin.y + visibleSize.height / 2));
+	this->addChild(bg);
+
+	//ÒôÐ§
+	auto soundOnMenuItem = MenuItemImage::create(
+		"menu/on.png",
+		"menu/on.png");
+	auto soundOffMenuItem = MenuItemImage::create(
+		"menu/off.png",
+		"menu/off.png");
+
+	auto soundToggleMenuItem = MenuItemToggle::createWithCallback(CC_CALLBACK_1(Setting::menuSoundToggleCallback, this),
+		soundOnMenuItem,
+		soundOffMenuItem,
+		NULL);
+	soundToggleMenuItem->setPosition(Director::getInstance()->convertToGL(Vec2(818, 220)));
+
+	//ÒôÀÖ
+	auto musicOnMenuItem = MenuItemImage::create(
+		"menu/on.png",
+		"menu/on.png");
+	auto musicOffMenuItem = MenuItemImage::create(
+		"menu/off.png",
+		"menu/off.png");
+	auto musicToggleMenuItem = MenuItemToggle::createWithCallback(CC_CALLBACK_1(Setting::menuMusicToggleCallback, this),
+		musicOnMenuItem,
+		musicOffMenuItem,
+		NULL);
+	musicToggleMenuItem->setPosition(Director::getInstance()->convertToGL(Vec2(818, 362)));
+
+	//Ok°´Å¥
+	auto okMenuItem = MenuItemImage::create(
+		"menu/ok-down.png",
+		"menu/ok-up.png",
+		CC_CALLBACK_1(Setting::menuOkCallback, this));
+
+	okMenuItem->setPosition(Director::getInstance()->convertToGL(Vec2(600, 510)));
+
+	Menu* mn = Menu::create(soundToggleMenuItem, musicToggleMenuItem, okMenuItem, NULL);
+	mn->setPosition(Vec2::ZERO);
+	this->addChild(mn);
+
+	return true;
+}
+
+void Setting::menuOkCallback(Ref* pSender)
+{
+	Director::getInstance()->popScene();
+}
+
+
+void Setting::menuSoundToggleCallback(Ref* pSender)
+{
+
+}
+
+
+void Setting::menuMusicToggleCallback(Ref* pSender)
+{
+
+}

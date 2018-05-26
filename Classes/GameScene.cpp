8e03de5b0f0de822360a26d6e6bccc52a7cc64be
ZhangBolun <7578973@qq.com>
2018-05-26 21:26:49 +0800

    增加游戏场景
    点击“开始游戏”进入游戏场景，方式为“replace”，清除菜单场景的缓存

---
+++ Classes/GameScene.cpp
@@ -0,0 +1,51 @@
+#include "GameScene.h"
+
+USING_NS_CC;
+
+Scene* Game::createScene()
+{
+	// 'scene' is an autorelease object
+	auto scene = Scene::create();
+
+	// 'layer' is an autorelease object
+	auto layer = Game::create();
+
+	// add layer as a child to scene
+	scene->addChild(layer);
+
+	// return the scene
+	return scene;
+}
+
+
+
+// on "init" you need to initialize your instance
+bool Game::init()
+{
+	//////////////////////////////
+	// 1. super init first
+	if (!Layer::init())
+	{
+		return false;
+	}
+
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	Sprite *bg = Sprite::create("menu/background.png");
+
+	// position the label on the center of the screen
+	bg->setPosition(Vec2(origin.x + visibleSize.width / 2,
+		origin.y + visibleSize.height / 2));
+	this->addChild(bg);
+
+
+
+
+	return true;
+}
+
+
+
+
+

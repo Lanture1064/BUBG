adc4980b683c3959948c4151f6b47a2df00add9f
Zhou <playerzhou@icloud.com>
2018-06-14 15:11:25 +0800

    add net test,it has it's own main function

---
+++ Classes/Net/NetConnectTest.cpp
@@ -0,0 +1,50 @@
+#include <boost\asio.hpp>
+#include <iostream>
+#include <thread>
+#include "Client.h"
+#include "Server.h"
+#include "NetData.h"
+
+int main()
+{
+	int temp;
+	std::cin >> temp;
+	if (temp == 1)
+	{
+		Server::getInstance()->beginWait();
+		std::thread t1(&Server::connect, (Server::getInstance()));
+		t1.detach();
+		std::thread t2(&Server::getCommand, (Server::getInstance()));
+		std::thread t3(&Server::replayCommand,(Server::getInstance()));
+		t2.detach();
+		t3.detach();
+		for (;;)
+		{
+			auto i = Server::getInstance()->getLocalCommand();
+			for (auto j = i.begin(); j != i.end(); ++j)
+			{
+				std::cout << j->command << std::endl;
+			}
+		}
+	}
+	else if (temp == 2)
+	{
+		Client::getInstance()->connect();
+		std::thread t1(&Client::getCommand,(Client::getInstance()));
+		std::thread t2(&Client::replayCommand,(Client::getInstance()));
+		t1.detach();
+		t2.detach();
+		for (;;)
+		{
+			auto i = Client::getInstance()->getLocalCommand();
+			for (auto j = i.begin(); j != i.end(); ++j)
+			{
+				std::cout << j->command << std::endl;
+			}
+			CommandImformation command;
+			command.command = DIRECTION;
+
+			Client::getInstance()->addNetCommand(command);
+		}
+	}
+}

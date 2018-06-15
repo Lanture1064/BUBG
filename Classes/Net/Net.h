daccd795ffce57f1101e5d7290085a52ea0bdbc8
Zhou <playerzhou@icloud.com>
2018-06-15 20:00:22 +0800

    add the head file of net

---
+++ Classes/Net/Net.h
@@ -0,0 +1,9 @@
+#ifndef BUBG_CLASSES_NET_NET_H_
+#define BUBG_CLASSES_NET_NET_H_
+
+#include "Client.h"
+#include "Server.h"
+#include "NetData.h"
+#include "NetControler.h"
+
+#endif // !BUBG_CLASSES_NET_NET_H_
